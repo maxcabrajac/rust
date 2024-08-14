910168251d7fa3a972029e34de8786f75c9b4f4a
maxcabrajac <max@cabrajac.com>
2024-08-14 17:51:31 -0300

    Unify fn_ret_ty

--- compiler/rustc_ast/src/visitors.rs
+++ compiler/rustc_ast/src/visitors.rs
@@ -60,9 +60,6 @@ macro_rules! mutability_dependent {
         fn visit_fn(&mut self, fk: FnKind<'ast>, _: Span, _: NodeId) -> Self::Result {
             walk_fn(self, fk)
         }
-        fn visit_fn_ret_ty(&mut self, ret_ty: &'ast FnRetTy) -> Self::Result {
-            walk_fn_ret_ty(self, ret_ty)
-        }
         fn visit_expr_field(&mut self, f: &'ast ExprField) -> Self::Result {
             walk_expr_field(self, f)
         }
@@ -411,6 +408,10 @@ macro_rules! make_ast_visitor {
 
             fn visit_vis(&mut self, vis: ref_t!(Visibility)) -> result!() {
                 walk_vis(self, vis)
+            }
+
+            fn visit_fn_ret_ty(&mut self, ret_ty: ref_t!(FnRetTy)) -> result!() {
+                walk_fn_ret_ty(self, ret_ty)
             }
 
             // FIXME: remove _ on ident if mut
@@ -1904,7 +1905,7 @@ pub mod mut_visit {
     fn walk_parenthesized_parameter_data<T: MutVisitor>(vis: &mut T, args: &mut ParenthesizedArgs) {
         let ParenthesizedArgs { inputs, output, span, inputs_span } = args;
         visit_thin_vec(inputs, |input| vis.visit_ty(input));
-        walk_fn_ret_ty(vis, output);
+        vis.visit_fn_ret_ty(output);
         vis.visit_span(span);
         vis.visit_span(inputs_span);
     }
@@ -2212,7 +2213,7 @@ pub mod mut_visit {
     fn walk_fn_decl<T: MutVisitor>(vis: &mut T, decl: &mut P<FnDecl>) {
         let FnDecl { inputs, output } = decl.deref_mut();
         inputs.flat_map_in_place(|param| vis.flat_map_param(param));
-        walk_fn_ret_ty(vis, output);
+        vis.visit_fn_ret_ty(output);
     }
 
     fn walk_fn_ret_ty<T: MutVisitor>(vis: &mut T, fn_ret_ty: &mut FnRetTy) {
